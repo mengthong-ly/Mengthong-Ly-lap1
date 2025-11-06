8ab8e1514184fc65e0d2bd766928b0c5b650ced9
KZE-THONG <155614637+KZE-THONG@users.noreply.github.com>
2025-11-06 14:42:01 +0700

    Fix ESLint config for v9+ and align tests

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "commonjs",
+      globals: {
+        // Node.js globals
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        exports: "writable",
+        module: "writable",
+        require: "readonly",
+        global: "readonly",
+        // Mocha globals for test files
+        describe: "readonly",
+        it: "readonly",
+        before: "readonly",
+        after: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly"
+      }
+    },
+    rules: {}
+  }
+];
